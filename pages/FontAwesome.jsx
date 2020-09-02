5712ce3aff61806f4cebd1debad0a8370aa6960d
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2020-09-02 23:03:07 +0900

    test FontAwesome

---
+++ pages/FontAwesome.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const FontAwesome = () => {
+    return(
+        <FontAwesomeIcon icon={faCoffee} />
+    )
+};
+
+export default FontAwesome;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import FontAwesome from './FontAwesome'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <FontAwesome />
         <h1 className={styles.title}>
           Lean to <a href="https://nextjs.org">Next.js!</a>
         </h1>
